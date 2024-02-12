1b460c729275c57cc63de6a2474928411550212a
Josh <Joschua.Schramm@yahoo.de>
2024-02-12 17:14:06 +0100

    added food typed, probability of getting sick as well as resistance

--- pytest-Genetic_Algorithm/MAINCODE/main.py
+++ pytest-Genetic_Algorithm/MAINCODE/main.py
@@ -10,16 +10,27 @@ from matplotlib.pyplot import imshow
 ENERGYCOSTS_MOVEMENT = 1
 ENERGYCOSTS_REPRODUCTION = 5
 START_ENERGY = 10
-WIDTH = 10
-HEIGHT = 10
+WIDTH = 100
+HEIGHT = 100
 NUMBER_AGENTS = 10
-ROUNDS = 21
-ENERGY_FOOD = 5
-FOOD_PERCENTAGE_BEGINNING = 0.5
-ADDITIONAL_FOOD_PERCENTAGE = 0.05
+ROUNDS = 10
+FOOD_PERCENTAGE_BEGINNING = 1
+ADDITIONAL_FOOD_PERCENTAGE = 0.5
+SICKNESS_DURATION = ROUNDS // 10
 
-# Globaler Counter für die Nummerierung der Lebewesen
+# Globaler Counter
 agents_counter = NUMBER_AGENTS
+
+FOOD = {
+    "1": {'Energy': 5, 'consume_time': 3, 'disease_risk': 0.0},  
+    "2": {'Energy': 10, 'consume_time': 6, 'disease_risk': 0.0},
+    "3": {'Energy': 15, 'consume_time': 9, 'disease_risk': 0.0},
+    "5": {'Energy': 20, 'consume_time': 12, 'disease_risk': 0.},
+    "4": {'Energy': 5, 'consume_time': 3, 'disease_risk': 0.0},
+    "6": {'Energy': 10, 'consume_time': 6, 'disease_risk': 0.0},
+    "7": {'Energy': 15, 'consume_time': 3, 'disease_risk': 0.0}   
+}
+FOOD_KEYS = list(FOOD.keys())
 
 # Genpool
 ## evtl. Gene mit Wahrscheinlichkeiten, zb. Mut 
@@ -27,19 +38,25 @@ GENPOOL = {
     "Genes": {
         "Kondition": (1, 3),
         "Visibilityrange": (1, 3),
-        "Tribe": (1, 3)
+        "Tribe": (1, 3),
+        "Resistance": (1, 5)
     }
 }
 
 class Agent:
-    def __init__(self, number):
+    def __init__(self, number, sick = 0):
         global agents_counter
+        self.sickness_counter = 0
         self.number = number
         self.energy = START_ENERGY
         self.genetic = {}
         self.genedistribution()
         self.position = (random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1))
         self.reproduction_counter = 0
+        self.consume_counter = 0
+        self.sick = False
+        self.sickness_duration = 0
+        self.previous_kondition = None
 
     def genedistribution(self):
         #simulieren des Genverteilung aus dem festgelegten Dictionarie 'GENPOOL'
@@ -47,32 +64,54 @@ class Agent:
                 # Ganzzahliger Wert für jedes Gen innerhalb der definierten Grenzen
                 self.genetic[gen] = random.randint(*bereich)
                 
+    def consuming_food(self, food_dict):
+        food = food_dict
+        risk = food["disease_risk"]
+        if random.random() < risk * (1 - self.genetic["Resistance"] / 5.0):
+            self.sick = True
+            self.sickness_duration = SICKNESS_DURATION
+            self.sickness_counter += 1
+            self.previous_kondition = self.genetic['Kondition']
+            self.genetic["Kondition"] = 1
+        self.energy += food["Energy"]
+        self.consume_counter += 1
 
     def move(self, board):
         if self.energy > ENERGYCOSTS_MOVEMENT:
-            self.energy -= ENERGYCOSTS_MOVEMENT
-            new_position = self.search_food(board)
-            if new_position:
-                self.position = new_position
+            if self.sick is True:
+                self.check_for_sickness()
+            
             else:
-                # Zufällige Bewegung: -1 oder 1, multipliziert mit der Kondition
-                dx = random.choice([-1, 1]) * self.genetic['Kondition']
-                dy = random.choice([-1, 1]) * self.genetic['Kondition']
-
-                new_x = max(0, min(WIDTH - 1, self.position[0] + dx))
-                new_y = max(0, min(HEIGHT - 1, self.position[1] + dy))
-                self.position = (new_x, new_y)
+                self.energy -= ENERGYCOSTS_MOVEMENT
+                new_position = self.search_food(board)
+                if new_position:
+                    self.position = new_position
+                else:
+                    # Zufällige Bewegung: -1 oder 1, multipliziert mit der Kondition
+                    dx = random.choice([-1, 1]) * self.genetic['Kondition']
+                    dy = random.choice([-1, 1]) * self.genetic['Kondition']
+                    new_x = max(0, min(WIDTH - 1, self.position[0] + dx))
+                    new_y = max(0, min(HEIGHT - 1, self.position[1] + dy))
+                    self.position = (new_x, new_y)
         else:
             return "deceased"
+
+    def check_for_sickness(self):
+        if self.sick:
+            self.sickness_duration -= 1
+            if self.sickness_duration <= 0:
+                self.sick = False
+                self.genetic["Kondition"] = self.previous_kondition
 
     def search_food(self, board):
         visibilityrange = self.genetic["Visibilityrange"]
         for dx in range(-visibilityrange, visibilityrange + 1):
             for dy in range(-visibilityrange, visibilityrange + 1):
                 x, y = self.position[0] + dx, self.position[1] + dy
-                if 0 <= x < WIDTH and 0 <= y < HEIGHT and board.food[x][y]:
-                    board.food[x][y] = 0
-                    self.energy += ENERGY_FOOD
+                if 0 <= x < WIDTH and 0 <= y < HEIGHT and board.food[x][y] is not None:
+                    food_dict = board.food[x][y]  # Schlüssel des Nahrungstyps
+                    self.consuming_food(food_dict)  # Aufrufen von consuming_food mit dem Schlüssel
+                    board.food[x][y] = None  # Entfernen der Nahrung von den Koordinaten
                     return (x, y)
         return None
 
@@ -104,7 +143,7 @@ class Board:
         self.width = width
         self.height = height
         self.agents_list = []
-        self.food = np.zeros((width, height))
+        self.food = np.full((width, height), None)
         self.world = np.zeros((width, height))
 
     def add_agent(self, agents_to_add):
@@ -113,12 +152,11 @@ class Board:
     def place_food(self, prozent):
         amount_fields = int(self.width * self.height * prozent)
         for _ in range(amount_fields):
-            
-            #random Koordinaten an dem die Nahrung pltziert werden soll
             x, y = random.randint(0, self.width - 1), random.randint(0, self.height - 1)
-            
-            #platzieren der Nahrung
-            self.food[x][y] = ENERGY_FOOD
+            food_type = random.choice(FOOD_KEYS)
+            food_dict = FOOD[food_type]
+            # Speichern des Nahrungstyps und der zugehörigen Werte direkt im food-Array
+            self.food[x][y] = food_dict
     
         
     def remove_agents(self, agent):
@@ -151,6 +189,7 @@ class Game:
                 if result == "deceased":
                     self.board.remove_agents(agent)
 
+
                 #wenn agent noch lebt wird die fortpflanzung weitergeführt
                     #vergleicht Agent mit potentiellen partnern
                 else:
@@ -182,14 +221,15 @@ class Game:
         # Schreibe die Agenten-Daten in die CSV-Datei
         with open(filename, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['Number', 'Tribe', 'Condition', 'Visibility Range', 'Reproduction Counter', 'Position'])
+            writer.writerow(['Number', 'Tribe', 'Condition', 'Visibility Range', 'Reproduction Counter', 'Consume Counter', 'Position'])
             for agent in self.board.agents_list:
                 writer.writerow([
                     agent.number, 
                     agent.genetic['Tribe'], 
                     agent.genetic['Kondition'], 
                     agent.genetic['Visibilityrange'], 
-                    agent.reproduction_counter, 
+                    agent.reproduction_counter,
+                    agent.consume_counter, 
                     agent.position
                 ])
 
@@ -200,8 +240,8 @@ class Game:
 
 if __name__ == "__main__":
     start = time.time()
-    #game = Game(saving=True)
-    game = Game()
+    game = Game(saving=True)
+    #game = Game()
     game.run()
-    timee = np.round(time.time() - start, 2)
-    print(timee)
+    script_time = np.round(time.time() - start, 2)
+    print(script_time)
