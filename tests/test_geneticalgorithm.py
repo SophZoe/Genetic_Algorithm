544ddfedd1067c4bf7862fedda036b6b10795843
Anna Popova <128530418+Salt-is-leaving@users.noreply.github.com>
2024-02-23 01:16:00 +0100

    Update test_geneticalgorithm.py

--- tests/test_geneticalgorithm.py
+++ tests/test_geneticalgorithm.py
@@ -1,7 +1,7 @@
 import pytest
-from src.package.class_Agent import Agent
-from src.package.class_Game import Game
-from src.package.class_Board import Board
+from src.package.class_agent import Agent
+from src.package.class_aame import Game
+from src.package.class_board import Board
 from src.main import GENPOOL, START_ENERGY, WIDTH, HEIGHT, ENERGYCOSTS_REPRODUCTION, ROUNDS, FOOD_PERCENTAGE_BEGINNING
 import os
 
